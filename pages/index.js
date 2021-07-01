d80f0c7e3ecfdc9acc974ddca2fdac3891a8d5ff
Ahmed Alsudairi <a7madalsudairi@gmail.com>
2021-07-01 17:15:26 +0300

    bulding MeetUpList component

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,23 @@
+import MeetUpList from "../components/meetups/MeetupList";
+
+const DUMMY_DATA = [
+  {
+    id: "m1",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/a/a8/Tour_Eiffel_Wikimedia_Commons.jpg",
+    title: "evil",
+    address: "somthing",
+    desciption: "somewhat",
+  },
+  {
+    id: "m2",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/a/a8/Tour_Eiffel_Wikimedia_Commons.jpg",
+    title: "taowe",
+    address: "somthing2",
+    desciption: "somewhat2",
+  },
+];
 export default function Home() {
-  return <h1>Home page</h1>;
+  return <MeetUpList meetups={DUMMY_DATA} />;
 }
