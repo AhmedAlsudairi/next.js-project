1f67eccda0b45ec4e017bda165262970c17304b9
Ahmed Alsudairi <a7madalsudairi@gmail.com>
2021-07-03 05:33:32 +0300

    implementing add new meetup

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,8 +1,13 @@
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
-
+import axios from "axios";
+import { useRouter } from "next/router";
 export default function Home() {
+  const router = useRouter();
+
   const onAddMeetup = (meetup) => {
-    console.log(meetup);
+    axios.post("/api/new-meetup", meetup).then((res) => {
+      console.log(res.data);
+    });
   };
   return <NewMeetupForm onAddMeetup={onAddMeetup} />;
 }
